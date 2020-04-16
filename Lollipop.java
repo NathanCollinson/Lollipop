19e0dcc56788c7442e4bfa4a347074e62b624166
Nathan Collinson <nathan.collinson@student.onslow.school.nz>
2020-04-16 12:00:20 +1200

    New Class Lollipop

--- Lollipop.java
+++ Lollipop.java
@@ -9,21 +9,29 @@ import java.io.*;
 import java.awt.Color;
 
 /** 
- * Write a description of class Lollipop here.
+ * Draw Little pictures on the graphics pane
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author: Nathan Collinson
+ * @version: 1
  */
 public class Lollipop{
 
     /**
-     * Constructor for objects of class Lollipop 
+     * Constructor for objects of class Lollipop
+     * Draw a red lollipop with a stick
      */
     public Lollipop(){
     UI.initialise();
     UI.addButton("Quit", UI::quit);    // Comment out to checkstyle
     }
-
+    private void drawLollipop(){
+        UI.setLineWidth(10);                //set line width to 10
+        UI.drawLine(300, 200, 300, 400);    //draw line     (300, 200) to (300, 400)
+        UI.setLineWidth(1);//set line width to 1
+        UI.setColor(Color.red);//set colour to red
+        UI.fillOval(260, 160, 80, 80);//fill oval     @(260, 100), (80*50)
+        
+    }
 
     /**
      * Main routine
@@ -31,6 +39,7 @@ public class Lollipop{
      */
     public static void main(String[] args){
         Lollipop obj = new Lollipop();
+        obj.drawLollipop();
     }
 
 }
